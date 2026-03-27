ca2d93df5509884e5dd2413678754a3162d56bcd
srimathi-sadhasivam <srimathisadhasivam@gmail.com>
2026-03-27 10:02:52 +0530

    changed dockerfile

--- backend/server.js
+++ backend/server.js
@@ -37,10 +37,10 @@ app.use('/api/inquiries', inquiryRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
